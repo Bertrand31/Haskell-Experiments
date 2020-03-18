6bcc99c0002e111507d07b0831078c749be7b3b5
Bertrand31 <bertrandjun@gmail.com>
2020-03-18 23:09:51 +0100

    Remove half-baked minesweeper

--- app/Main.hs
+++ app/Main.hs
@@ -2,7 +2,6 @@ module Main (main) where
 
 import System.Random
 import Control.Exception (assert)
-import MineSweeper
 import MergeSort (mergeSort)
 import GCD (gcd')
 import qualified BloomFilter
@@ -10,19 +9,6 @@ import qualified Bitset
 
 writeOk :: String -> IO ()
 writeOk fnName = putStrLn $ fnName ++ " [OK]"
-
-pp :: Result -> IO ()
-pp x = putStr (concat (map (++"\n") x)) --prints strings in a column.
-
-
-sampleInput = ["       ",
-               " *     ",
-               "    *  ",
-               "   *   ",
-               "      *",
-               "***    ",
-               "* *    ",
-               "***    "]
 
 main :: IO ()
 main = do
@@ -35,7 +21,6 @@ main = do
   assert (BloomFilter.null bloomFilterWithFoo == False) writeOk "BloomFilter null"
   assert (BloomFilter.member "foo" bloomFilterWithFoo == True) writeOk "BloomFilter member"
   assert (BloomFilter.member "bar" bloomFilterWithFoo == False) writeOk "BloomFilter member"
-  pp $ minesweeper sampleInput
   let bitset = Bitset.empty
   let bitset1 = Bitset.insert bitset 5
   let bitset2 = Bitset.insert bitset1 50
