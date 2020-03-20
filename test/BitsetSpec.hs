8f947a08afc46e440224843f5a6eef64614d7019
Bertrand31 <bertrandjun@gmail.com>
2020-03-20 09:09:23 +0100

    Add more bitset specs

--- test/BitsetSpec.hs
+++ test/BitsetSpec.hs
@@ -12,6 +12,10 @@ spec :: Spec
 spec = do
   let emptyBs = Bitset.empty
 
+  describe "Bitset.null" $ do
+    it "should say an empty bitset is empty" $ do
+      Bitset.null emptyBs `shouldBe` True
+
   describe "Bitset.insert" $ do
     it "should insert an integer into a bitset" $ do
       let bs = Bitset.insert emptyBs 5
@@ -22,7 +26,31 @@ spec = do
       let bs = Bitset.insertMany emptyBs [5, 32, 1]
       Bitset.size bs `shouldBe` (3 :: Int)
 
-  -- assert (gcd' 48 64 == 16) writeOk "GCD"
+  let populatedBs = Bitset.insertMany emptyBs [5, 31, 32, 62, 63, 64, 1030193]
+
+  describe "Bitset.member" $ do
+    it "should return true for numbers that have been inserted" $ do
+      Bitset.member populatedBs 5 `shouldBe` True
+      Bitset.member populatedBs 31 `shouldBe` True
+
+    it "should return false for numbers that have not been inserted" $ do
+      Bitset.member populatedBs 33 `shouldBe` False
+
+  describe "Bitset.size" $ do
+    it "should return the correct cardinality of the bitset" $ do
+      Bitset.size populatedBs `shouldBe` (7 :: Int)
+
+  describe "Bitset.toList" $ do
+    it "should return a list of integers stored by the bitset" $ do
+      Bitset.toList populatedBs `shouldBe` [5, 31, 32, 62, 63, 64, 1030193]
+
+  describe "Bitset.delete" $ do
+    it "should delete an integer from the bitset" $ do
+      let bitsetMinus5 = Bitset.delete populatedBs 5
+      Bitset.size bitsetMinus5 `shouldBe` (6 :: Int)
+      Bitset.member bitsetMinus5 5 `shouldBe` False
+
+
   -- assert (mergeSort (<) [7,1,6,4,2,9] == [1,2,4,6,7,9]) writeOk "MergeSort"
   -- gen <- getStdGen
   -- let bloomFilter = BloomFilter.empty 100000 0.0001 gen
@@ -31,17 +59,6 @@ spec = do
   -- assert (not $ BloomFilter.null bloomFilterWithFoo) writeOk "BloomFilter null"
   -- assert (BloomFilter.member "foo" bloomFilterWithFoo) writeOk "BloomFilter member"
   -- assert (not $ BloomFilter.member "bar" bloomFilterWithFoo) writeOk "BloomFilter member"
-  -- let bitset = Bitset.empty
-  -- assert (Bitset.null bitset) writeOk "Bitset null"
-  -- assert (not $ Bitset.null bitset) writeOk "Bitset null"
-  -- let bitset2 = Bitset.insertMany bitset [5, 31, 32, 62, 63, 64, 1030193]
-  -- assert (Bitset.member bitset2 5) writeOk "Bitset member"
-  -- assert (Bitset.member bitset2 31) writeOk "Bitset member"
-  -- assert (not $ Bitset.member bitset2 33) writeOk "Bitset member"
-  -- assert (Bitset.size bitset2 == 6) writeOk "Bitset size"
-  -- assert (Bitset.toList bitset2 == [5, 31, 32, 62, 63, 64, 1030193]) writeOk "Bitset toList"
-  -- let bitset3 = Bitset.delete bitset2 5
-  -- assert (not $ Bitset.member bitset3 5) writeOk "Bitset delete"
   -- let bs1 = Bitset.insert Bitset.empty 8
   -- let bs2 = Bitset.insert Bitset.empty 16
   -- let bs3 = Bitset.empty
